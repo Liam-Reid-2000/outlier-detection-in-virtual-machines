7311d22f5ed6b8f34dc6b7e323c5ee5336630c33
Liam Reid <lreid29@qub.ac.uk>
2022-02-22 13:04:17 +0000

    Setting threshold based on standard deviation for moving median

--- ensemble_detectors/moving_median_detection.py
+++ ensemble_detectors/moving_median_detection.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import statistics
+import numpy as np
 
 
 def get_median(arr):
     return statistics.median(arr)
 
+def find_threshold(arr):
+    return np.std(arr)
 
 def get_moving_median_coordinates(median_interval, data_points):
     
@@ -37,9 +40,12 @@ def detect_median_outliers(threshold, median_points, data_points):
     points_x = data_points['points_x']
     points_y = data_points['points_y']
 
+    bound = (find_threshold(points_y))
+
     i = 0
     while i < len(median_points_x):
-        if ((points_y[i] < median_points_y[i]-int(threshold)) or (points_y[i] > median_points_y[i]+int(threshold))):
+        #if ((points_y[i] < median_points_y[i]-int(threshold)) or (points_y[i] > median_points_y[i]+int(threshold))):
+        if ((points_y[i] < (median_points_y[i]-int(bound))) or (points_y[i] > (median_points_y[i]+int(bound)))):
             detected_ouliters_x.append(points_x[i])
             detected_ouliters_y.append(points_y[i])
         i += 1
