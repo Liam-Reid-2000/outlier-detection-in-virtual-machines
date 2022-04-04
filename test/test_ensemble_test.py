4702f97cdfe717747d0b283ce6dc977ba5158076
Liam Reid <lreid29@qub.ac.uk>
2022-04-04 13:44:29 +0100

    Fixing unit test for ensemble

--- test/test_ensemble_test.py
+++ test/test_ensemble_test.py
@@ -1,21 +1,21 @@
 import unittest
 
-from ensemble_detectors.moving_median_detection import get_median
-from ensemble_detectors.moving_average_detection import get_average
+from ensemble_detectors.moving_median_detection import moving_median_detection
+from ensemble_detectors.moving_average_detection import moving_average_detection
 
 class outlier_detection_test(unittest.TestCase):
 
     def test_average(self):
         test_array = [1,2,3,4,5]
-        self.assertEqual(3, get_average(test_array))
+        self.assertEqual(3, moving_average_detection.get_average(test_array))
         test_array_2 = [1,2,2,3,3,4,4,4,4]
-        self.assertEqual(3, get_average(test_array_2))
+        self.assertEqual(3, moving_average_detection.get_average(test_array_2))
 
     def test_median(self):
         test_array = [1,2,3,4,5]
-        self.assertEqual(3, get_median(test_array))
+        self.assertEqual(3, moving_median_detection.get_median(test_array))
         test_array_2 = [1,2,2,3,3,4,4,4,4]
-        self.assertEqual(3, get_median(test_array_2))
+        self.assertEqual(3, moving_median_detection.get_median(test_array_2))
 
 
 if __name__ == '__main__':
