b7ff4a91271b70bf64a92c7ebcba938d9e90c4da
Liam Reid <lreid29@qub.ac.uk>
2022-04-04 13:45:43 +0100

    Fixing unit test for average

--- test/test_moving_average_detection.py
+++ test/test_moving_average_detection.py
@@ -1,18 +1,18 @@
 import unittest
 
-from ensemble_detectors.moving_average_detection import is_data_outside_bounds
+from ensemble_detectors.moving_average_detection import moving_average_detection
 
 class outlier_detection_test(unittest.TestCase):
 
 
     def test_is_data_outside_bounds(self):
-        test_result = is_data_outside_bounds(1,3,1)
+        test_result = moving_average_detection.is_data_outside_bounds(1,3,1)
         self.assertEqual(True, test_result)
 
-        test_result = is_data_outside_bounds(2,2,5)
+        test_result = moving_average_detection.is_data_outside_bounds(2,2,5)
         self.assertEqual(False, test_result)
 
-        test_result = is_data_outside_bounds(7,2,2)
+        test_result = moving_average_detection.is_data_outside_bounds(7,2,2)
         self.assertEqual(True, test_result)
 
 if __name__ == '__main__':
