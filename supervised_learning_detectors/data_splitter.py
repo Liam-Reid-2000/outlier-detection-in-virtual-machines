3e4f15f907b194df3c41bb6a5383b62488075998
Liam Reid <lreid29@qub.ac.uk>
2022-04-19 17:24:51 +0100

    Removing unneed methods from supervised learning to increase test coverage

--- supervised_learning_detectors/data_splitter.py
+++ supervised_learning_detectors/data_splitter.py
@@ -47,7 +47,6 @@ def load_data(dataset, split_ratio):
     return split_data
 
 
-
 def split_outliers(target_data, split_date):
 
     train_outliers = []
@@ -71,18 +70,6 @@ def split_outliers(target_data, split_date):
     return outlier_data
 
 
-def pair_outliers(test_points_x, test_points_y, all_outliers):
-    outlier_y = []
-    i = 0
-    while (i < len(test_points_x)):
-        for j in all_outliers:
-            if (str(j) == str(test_points_x[i])):
-                outlier_y.append(test_points_y[i])
-        i+=1
-    return pd.DataFrame({'timestamp': all_outliers,'data': outlier_y})
-
-
-
 def convert_time_data_to_minutes_of_day(points_x):
     points_x_as_minutes = []
     for i in points_x:
@@ -94,32 +81,6 @@ def convert_time_data_to_minutes_of_day(points_x):
 def get_data_as_matrix(points_x, points_y):
     return(np.r_['1,2,0', points_x, points_y])
 
-    
-
-def get_outliers(test_points_x, test_points_y, test_outliers):
-    outlier_points_x = []
-    df = pair_outliers(test_points_x, test_points_y, test_outliers)
-    for i in df['timestamp']:
-        time = datetime.strptime(i, '%Y-%m-%d %H:%M:%S')
-        outlier_points_x.append(time)
-    return(np.r_['1,2,0', outlier_points_x, df['data']])
-
-
-def get_outlier_areas(outliers_x, target_data):
-    f = open('resources/combined_windows.json')
-    data = json.load(f)
-    arrX1 = []
-    arrX2 = []
-    for i in data[target_data]:
-        x1 = datetime.strptime(i[0], '%Y-%m-%d %H:%M:%S.%f')
-        x2 = datetime.strptime(i[1], '%Y-%m-%d %H:%M:%S.%f')
-        for j in outliers_x:
-            j = datetime.strptime(j, '%Y-%m-%d %H:%M:%S')
-            if (j > x1 and j < x2):
-                arrX1.append(x1)
-                arrX2.append(x2)
-    f.close()
-    return pd.DataFrame({'first_x': arrX1, 'second_x': arrX2})
 
 
 def remove_outliers_from_training_data(train_outliers, train_points_x, train_points_y):
